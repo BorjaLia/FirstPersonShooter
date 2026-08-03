f55cd735221c3f10bab7fc5959cad274dc9db096
BorjaLia <borja.lia.deichler@gmail.com>
2026-08-03 05:29:00 -0300

    Frogs added

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -66,7 +66,7 @@ public class UIManager : MonoBehaviour
     {
         if (progressText != null)
         {
-            progressText.text = $"Enemies: {totalEnemies - enemiesDefeated}";
+            progressText.text = $"Enemies: {enemiesDefeated} / {totalEnemies}";
         }
     }
 }
